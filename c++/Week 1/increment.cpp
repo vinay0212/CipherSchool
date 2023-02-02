9fbd53066912f118ca8ffe9766125a8084d6b67d
Vinay Appikonda <116058436+vinay0212@users.noreply.github.com>
2023-02-02 17:29:31 +0530

    Add files via upload

---
+++ c++/Week 1/increment.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i=0;
+
+    i=11;
+    i++;
+
+    cout<<i<<endl;
+
+    return 0;
+}
+
---
+++ c++/Week 2/mode.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// single
+class A{
+    int a;
+    protected:
+    int b;
+    public:
+    int c;
+};
+class B: public A{
+    //private protected:
+    // int b;
+    //private public:
+    // int c;
+    public:
+};
+
+
+int main(){
+    // A obj;
+    B obj;
+    // cout<<obj.a<<obj.b<<obj.c<<endl;
+
+
+    // cout<<obj.a<<obj.b<<obj.c<<endl;
+
+  return 0;
+}
